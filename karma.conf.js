805cf690ccc778ca7cbd83c2e90fe158c3ee20e9
Conrad VanLandingham <cv@utexas.edu>
2015-12-13 01:19:41 -0600

    initial commit

---
+++ karma.conf.js
@@ -0,0 +1,76 @@
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var path = require('path');
+var webpack = require('webpack');
+
+var withCoverage = process.argv.indexOf('coverage') !== -1 || process.env.COVERAGE;
+
+var webpackConfig = {
+  devtool: 'eval',
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: withCoverage ?
+      [
+        {test: /\.js$/, loader: 'babel', include: [path.resolve('./test')]},
+        {test: /\.js$/, loader: 'isparta', include: [path.resolve('./src')]},
+      ] :
+      [
+        {
+          test: /\.js$/, loader: 'babel', include: [path.resolve('./src'), path.resolve('./test')],
+        },
+      ],
+  },
+  stats: {
+    colors: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+};
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'node_modules/babel-core/browser-polyfill.js',
+      'test/index.js',
+    ],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: {
+        chunkModules: false,
+        colors: true,
+      },
+    },
+    exclude: [],
+    preprocessors: {
+      'test/index.js': ['webpack'],
+    },
+    reporters: ['progress'],
+    coverageReporter: {
+      dir: './coverage/',
+      subdir: '.',
+      reporters: [
+        {type: 'html'},
+        {type: 'lcovonly'},
+        {type: 'text', file: 'text.txt'},
+        {type: 'text-summary', file: 'text-summary.txt'},
+      ],
+    },
+    captureTimeout: 90000,
+    browserNoActivityTimeout: 60000,
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+  });
+};
